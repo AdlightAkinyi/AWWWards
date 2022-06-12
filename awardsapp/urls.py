f173bd4b8826ea382db788ebe496a7545fdcb375
C0159412020 <adlightakinyi1@gmail.com>
2022-06-12 19:15:30 +0300

    updated urls

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.contrib.auth import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^',include('awwardsapp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page":'/'}),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+
 ]
