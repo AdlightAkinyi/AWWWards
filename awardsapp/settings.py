9bfa51b3a4c9ff8debde1140cd3058fe46ae879a
C0159412020 <adlightakinyi1@gmail.com>
2022-06-14 21:43:38 +0300

    deployed

--- awardsapp/settings.py
+++ awardsapp/settings.py
@@ -23,15 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rq4am6dmgvvy=(f9qib6huw777x)j(qh%k1^+h&cpj#n$&x@0r'
+
 # SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-ALLOWED_HOSTS=[]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
