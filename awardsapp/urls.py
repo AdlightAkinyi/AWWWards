ad6ba2222985e7b52acb48f38eac340ee18f21ce
C0159412020 <adlightakinyi1@gmail.com>
2022-06-14 23:24:36 +0300

    updated urls

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 # from django.urls import path
 from django.contrib.auth import views
-from django.contrib.auth.views import LogoutView
+
 
 
 urlpatterns = [
