39409b36f45c6e24d3c395ef411907b85612cb58
C0159412020 <adlightakinyi1@gmail.com>
2022-06-14 22:39:50 +0300

    updated urls

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 
 # from django.conf.urls import url
-from django.urls import include, path 
+from django.urls import include, re_path as url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+# from django.urls import path
 from django.contrib.auth import views
 from django.contrib.auth.views import LogoutView
-from django_registration.backends.one_step.views import RegistrationView
 
 
 urlpatterns = [
@@ -32,13 +32,12 @@ urlpatterns = [
     # #  url(r'^accounts/', include('registration.backends.simple.urls')),
     # url('logout/', auth_views.LogoutView.as_view(next_page = '/')),
     # url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
-    path('admin/', admin.site.urls),
-    path('',include('awardsproject.urls')),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('awardsproject.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     # url('logout/', auth_views.LogoutView.as_view(next_page = '/')),
     # url(r'^logout/$', views.logout,{"next_page":'/'}),
-  
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
 ]
 
 if settings.DEBUG: 
--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -1,18 +1,18 @@
 # from django.conf.urls import url
-from django.urls import include, path 
+from django.urls import re_path as url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('$',views.index,name = 'index'),
-    path('new/post$', views.new_post, name='new-post'),
-    path('profile/(?P<user_id>\d+)?$', views.profile, name='profile'),
-    path('update/profile$', views.updateprofile, name='updateprofile'),
-    path('vote/(?P<post_id>\d+)?$', views.vote, name='vote'),    
-    path('search/', views.search_results, name='search_results'),
-    path('api/post/$', views.PostList.as_view()),
-    path('api/profile/$', views.ProfileList.as_view()),
+    url('^$',views.index,name = 'index'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^profile/(?P<user_id>\d+)?$', views.profile, name='profile'),
+    url(r'^update/profile$', views.updateprofile, name='updateprofile'),
+    url(r'^vote/(?P<post_id>\d+)?$', views.vote, name='vote'),    
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/post/$', views.PostList.as_view()),
+    url(r'^api/profile/$', views.ProfileList.as_view()),
 ]
 if settings.DEBUG: 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
